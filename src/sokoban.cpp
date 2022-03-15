ef143b1b0fd9156d866a8dd9b1957a37dc643ea0
Nireves333 <85760266+Nireves333@users.noreply.github.com>
2022-03-15 11:25:14 -0700

    Clean up sokoban.cpp

--- src/sokoban.cpp
+++ src/sokoban.cpp
@@ -1,21 +1,19 @@
 #include "sokoban.hpp"
+#include <stdexcept>
 
-Sokoban::Sokoban(std::vector<std::vector<std::string>> levels) 
-{
+Sokoban::Sokoban(std::vector<std::vector<std::string>> levels) {
     this->levels = levels;
     current_level = 0;
     _board = levels.at(current_level);
 }
 
-unsigned int Sokoban::level() 
-{
+unsigned int Sokoban::level() {
     return current_level;
 }
 
-bool Sokoban::solved() 
-{
-    for (std::string row : board()) {
-        for (char cell : row) {
+bool Sokoban::solved() {
+    for (const std::string &row : board()) {
+        for (const char cell : row) {
             if (cell == '.' || cell == '+') {
                 return false;
             }
@@ -24,26 +22,20 @@ bool Sokoban::solved()
     return true;
 }
 
-std::vector<std::string> Sokoban::board() 
-{
+std::vector<std::string> Sokoban::board() {
     return _board;
 }
 
+void Sokoban::locate_player() {
+    for (int y = 0; y < _board.size(); y++) {
+        for (int x = 0; x < _board[y].size(); x++) {
+            if (_board[y][x] == '@' || _board[y][x] == '+') {
+			    px = x; 
+                py = y;
+			    return;
+            }
+        }
+    }
 
-void Sokobann::locate_player()
-{
-	for (int y = 0; y < _board.size(); y++)
-	{
-		for (int x = 0; x < _board[y].size(); x++)
-		{
-			if (_board[y][x] == '@' || _board[y][x] == '+')
-			{
-				px = x; py = y;
-				return;
-			}
-		}
-	}
-	
-	throw std::invalid_argument("Player not found...");
-	
+    throw std::invalid_argument("Player not found...");
 }
